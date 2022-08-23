8ae9da70a5c96e50b71c823335d1f6808dc1ddda
DHIRAJ PATIL <ardhirajrp@gmail.com>
2022-08-24 01:41:56 +0530

    Add files via upload

---
+++ scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+public Rigidbody rb; 
+public float ForwardForce = 2000f;
+public float SidewaysForce = 2000f;
+
+    
+    void FixedUpdate()
+    {
+
+        rb.AddForce (0,0,ForwardForce * Time.deltaTime, ForceMode.VelocityChange);
+
+        
+
+        if ( Input.GetKey("d"))
+        {
+            rb.AddForce  (SidewaysForce * Time.deltaTime ,0 ,0, ForceMode.VelocityChange);
+        }
+
+        if ( Input.GetKey("a"))
+        {
+            rb.AddForce  (-SidewaysForce * Time.deltaTime ,0 ,0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType <GameManager>().EndGame();
+        }
+
+        }
+
+    }
+
